996402f4643cb3dafcda639d71bad3c5c301506e
MatuAyerza <matu.ayerza@gmail.com>
2021-03-30 11:44:57 -0300

    project pushed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import "./App.css";
+import React from "react";
+// import fs from 'fs'
+
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
